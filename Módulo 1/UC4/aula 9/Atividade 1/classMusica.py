528ae0de17ab1299b0f930b33b56db51d6c89002
amorimry <274507430+amorimry@users.noreply.github.com>
2026-06-05 20:24:33 -0300

    novo

---
+++ Módulo 1/UC4/aula 9/Atividade 1/classMusica.py
@@ -0,0 +1,28 @@
+class Musica():
+    def __init__(self, nome, artista, genero, lancamento, album = "Não se enquadra"):
+        self.nome = nome
+        self.artista = artista
+        self.genero = genero
+        self.lacamento = lancamento
+        self.album = album
+
+    def detalhes_da_musica(self):
+        print(f"""
+--> Detalhes da Música
+        
+        Título: {self.nome}
+        Artista: {self.artista}
+        Gênero: {self.genero}
+        Ano de Lançamento: {self.lacamento}
+        Álbum: {self.album}
+""")
+        
+    def ver_plataforma(self):
+        if self.artista in ["Lana Del Rey", "Billie Eilish", "Mitski", "Harry Styles"]:
+            return "Ouça a música em: Spotify"
+        elif self.artista in ["Willow", "Conan Gray", "YUNGBLUD", "Grunge"]:
+            return "Ouça a música em: Apple Music"
+        elif self.artista in ["Olivia Rodrigo", "Kate Bush", "The Neighbourhood"]:
+            return "Ouça a música em: YouTube Music"
+        else:
+            return "Música não encontrada."
