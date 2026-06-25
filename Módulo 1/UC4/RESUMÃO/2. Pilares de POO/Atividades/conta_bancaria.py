3b6c0a0f6aff986cf96b11773de147bf198cc3ea
Ryan <274507430+amorimry@users.noreply.github.com>
2026-06-25 11:49:04 -0300

    finalizado os estudos sobre herança, getters e setters

--- Módulo 1/UC4/RESUMÃO/2. Pilares de POO/Atividades/conta_bancaria.py
+++ Módulo 1/UC4/RESUMÃO/2. Pilares de POO/Atividades/conta_bancaria.py
@@ -3,16 +3,18 @@ class ContaBancaria():
         self._titular = titular
         self.__saldo = saldo
 
-# GETTER (ermite ler o saldo de fora com: conta1.saldo)
+# GETTER (permite ler o saldo de fora com: conta1.saldo)
     @property
-    def ver_saldo(self):
+    def saldo(self):
         return self.__saldo
     
 # SETTER (permite alterar o saldo de fora com: conta1.saldo = valor)
-    @ver_saldo.setter
+    @saldo.setter
     def saldo(self, novo_saldo):
         if novo_saldo < 0:
             print("O saldo não pode ser negativo.")
+        elif novo_saldo > 20*self.__saldo:
+            print("Saldo ultrapassou o valor para altereação.")
         else:
             self.__saldo = novo_saldo
             print("Saldo atualizado.")
@@ -49,17 +51,35 @@ if __name__ == "__main__":
     conta1 = ContaBancaria("Pedro", 2000)
 
     conta1.ver_informacoes()
-
+    conta1.titula = "Ryan"
+        # não funciona porque não existe um atributo chamado titular, o que existe é _titular. aqui foi criado um novo atributo
     conta1._titular = "Ryan"
-    conta1.__saldo = 5000000000
+        # quando você usa um underscore (self._titular), isso é apenas uma convenção; significa: “esse atributo não deveria ser acessado diretamente fora da classe”, mas o Python não impede, por isso conta1._titular = "Ryan" funciona
+    conta1.__saldo = 5000000000 # não ocorre alteração pois foi criado um novo atributo, já que o atributo saldo só pode ser acessado para ver por meio do getter e acessado para alterar por meio do setter
     conta1.ver_informacoes()
-
+    conta1.saldo = -500000000 # setter usa-se o = ; ERRO
+    conta1.saldo = 100 # altera pois eu chamei o setter para alterar e validar, não estou adicionando nem retirando valores, com deposito ou saque, estou alterando diretamente o saldo da conta da pessoa (mantendo ainda as validações necessárias)
+    conta1.ver_informacoes()
+    conta1.sacar(50) # método usa-se os ()
+    conta1.ver_informacoes()
     conta1.depositar(0)
     conta1.depositar(100)
-
     conta1.ver_informacoes()
-
     conta1.sacar(10000)
-    conta1.sacar(1000)
-
+    conta1.sacar(20)
     conta1.ver_informacoes()
+    print(conta1.__saldo) # isso aqui ta puxando as informações de um novo atributo que eu criei, nada a ver com o original
+    print(conta1.saldo) # chama o getter para poder me mostrar o valor do atributo real
+
+
+    # OBS:
+        # PERGUNTA:
+        # mas se no setter eu coloco def e peço um valor dentro dos parenteses, pq quando eu vou alterar eu uso o = e não faço como se fosse um método? pq os métodos se vc quer um valor a mais vc pede dentro dos parenteses após o self, ai vc puxa o método e depois bota nos parenteses o seu valor a parte
+
+        # RESPOSTA:
+        # Isso é justamente a “mágica” do decorador @property em Python. Ele transforma um método em algo que se comporta como um atributo.
+        # Normalmente, um método é chamado com parênteses: obj.metodo(valor).
+        # Mas quando você usa @property e @atributo.setter, o Python faz um “encapsulamento elegante”:
+            # O getter permite acessar como se fosse um atributo: conta1.saldo (sem parênteses).
+            # O setter permite atribuir com =: conta1.saldo = 500.
+        # Ou seja, você escreve como se fosse um atributo simples, mas por trás o Python está chamando o método que você definiu. Isso é feito para deixar o código mais natural e legível.
