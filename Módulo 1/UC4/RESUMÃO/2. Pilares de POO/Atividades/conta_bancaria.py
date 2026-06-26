277c69f802e808716840104dd4383364179f9bd5
Ryan <274507430+amorimry@users.noreply.github.com>
2026-06-26 11:29:31 -0300

    melhoria do código

--- Módulo 1/UC4/RESUMÃO/2. Pilares de POO/Atividades/conta_bancaria.py
+++ Módulo 1/UC4/RESUMÃO/2. Pilares de POO/Atividades/conta_bancaria.py
@@ -1,7 +1,18 @@
-class ContaBancaria():
-    def __init__(self, titular, saldo):
+from abc import ABC, abstractmethod
+
+class BancoAbstracao(ABC):
+    pass
+
+    @abstractmethod
+    def fazer_login(self):
+        pass
+
+class ContaBancaria(BancoAbstracao):
+    def __init__(self, titular, saldo, senha, status = False):
         self._titular = titular
         self.__saldo = saldo
+        self.__senha = senha
+        self.status = status
 
 # GETTER (permite ler o saldo de fora com: conta1.saldo)
     @property
@@ -19,32 +30,58 @@ class ContaBancaria():
             self.__saldo = novo_saldo
             print("Saldo atualizado.")
 
+    def fazer_login(self):
+        while True:
+            senha = input("Digite sua senha: ")
+            if senha == self.__senha:
+                print("Senha correta, acesso liberado.")
+                self.status = True
+                break
+            else:
+                print("Senha incorreta.")
+                self.status = False
+
     def ver_informacoes(self):
-        print(f"""
+        if self.status:
+            print(f"""
 == INFORMAÇÕES DA CONTA ==
 
         Titular: {self._titular}
         Saldo: R$ {self.__saldo:,.2f}
 """)
+        else:
+            print("""
+    == FAÇA LOGIN NA SUA CONTA ==
+""")
         
     def depositar(self, valor):
-        if valor <= 0:
-            print("Valor inválido para deposito.")
+        if self.status:
+            if valor <= 0:
+                print("Valor inválido para deposito.")
 
+            else:
+                self.__saldo += valor
+                print(f"Deposito de R$ {valor:,.2f} efetuado com sucesso.")
         else:
-            self.__saldo += valor
-            print(f"Deposito de R$ {valor:,.2f} efetuado com sucesso.")
+            print("""
+    == FAÇA LOGIN NA SUA CONTA ==
+""")
 
     def sacar(self, valor):
-        if valor > self.__saldo:
-            print("Saldo insuficiente.")
-        
-        elif valor <= 0:
-            print("Valor inválido para saque.")
+        if self.status:
+            if valor > self.__saldo:
+                print("Saldo insuficiente.")
+            
+            elif valor <= 0:
+                print("Valor inválido para saque.")
 
+            else:
+                self.__saldo -= valor
+                print(f"Saque de R$ {valor:,.2f} efetuado com sucesso.")
         else:
-            self.__saldo -= valor
-            print(f"Saque de R$ {valor:,.2f} efetuado com sucesso.")
+            print("""
+    == FAÇA LOGIN NA SUA CONTA ==
+""")
 
 if __name__ == "__main__":
 
