337d0aac212f4727607dae843af7a4db2eee9625
amorimry <274507430+amorimry@users.noreply.github.com>
2026-07-24 20:31:22 -0300

    ajuste nas variáveis

--- Módulo 1/UC4/aula 24 EXTRA/atividade_2.py
+++ Módulo 1/UC4/aula 24 EXTRA/atividade_2.py
@@ -12,33 +12,35 @@ class Veiculo(ABC):
         pass
 
 class Caminhao(Veiculo):
-    def __init__(self, placa, marca, modelo, ano, carga_toneladas, km_rodado):
+    def __init__(self, placa, marca, modelo, ano, carga_toneladas):
         super().__init__(placa, marca, modelo, ano)
+        self.custo_basico = 470
         self.carga_toneladas = carga_toneladas
-        self.km_rodado = km_rodado
 
     def custo_viagem(self):
-        return (self.km_rodado * 35) + (self.carga_toneladas * 470)
+        return self.carga_toneladas * self.custo_basico
 
 class Onibus(Veiculo):
     def __init__(self, placa, marca, modelo, ano, qtd_passageiros):
         super().__init__(placa, marca, modelo, ano)
+        self.custo_basico = 5.40
         self.qtd_passageiros = qtd_passageiros
 
     def custo_viagem(self):
-        return self.qtd_passageiros * 5.40
+        return self.qtd_passageiros * self.custo_basico
 
 class Taxi(Veiculo):
     def __init__(self, placa, marca, modelo, ano, bandeira, km_rodado):
         super().__init__(placa, marca, modelo, ano)
+        self.custo_basico = 10
         self.bandeira = bandeira
         self.km_rodado = km_rodado
 
     def custo_viagem(self):
-        return self.bandeira + (self.km_rodado * 10)
+        return self.bandeira + (self.km_rodado * self.custo_basico)
 
 
-teste1 = Caminhao("ABC-1234", "Volvo", "FH", 2022, carga_toneladas=4, km_rodado=20)
+teste1 = Caminhao("ABC-1234", "Volvo", "FH", 2022, carga_toneladas=4)
 teste2 = Onibus("XYZ-5678", "Mercedes", "Marcopolo", 2020, qtd_passageiros=45)
 teste3 = Taxi("KGB-9999", "Chevrolet", "Spin", 2023, bandeira=25, km_rodado=3)
 
