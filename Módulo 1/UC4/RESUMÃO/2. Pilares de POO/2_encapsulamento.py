cc5e5919fa694f76698e154ed240cae8b7c82ed3
amorimry <274507430+amorimry@users.noreply.github.com>
2026-07-24 21:03:55 -0300

    final

--- Módulo 1/UC4/RESUMÃO/2. Pilares de POO/2_encapsulamento.py
+++ Módulo 1/UC4/RESUMÃO/2. Pilares de POO/2_encapsulamento.py
@@ -36,17 +36,18 @@ if __name__ == "__main__":
 
     pessoa1.apresentar_pessoa()
 
-
-
 # == DICA:
 # Pense no botão de volume de uma televisão: O circuito elétrico e a voltagem que passa nos alto-falantes são os atributos privados. Eles ficam escondidos dentro da carcaça da TV. O botão físico (ou o controle remoto) é o método público (o Setter). Se os atributos fossem públicos, seria o equivalente a você abrir a TV com uma chave de fenda e mexer direto nos fios elétricos para aumentar o som. Você poderia tomar um choque ou queimar a TV.Com o encapsulamento, você gira o botão. A TV (classe) recebe o seu pedido, valida se o volume não passou do máximo (segurança) e altera o circuito interno por você.
 
-
-
 # == GETTERS E SETTERS ==
+
 # são métodos que servem de "porteiros" de um atributo privado
+
 # getter = pegador
-    # serve apenas para ler o valor do atributo, ele não altera nada, apenas pega o valor e te entrega.
+    # serve apenas para ler o valor do atributo, ele não altera nada, apenas pega o valor e te entrega com segurança
+    # def get_cpf(self):
+        # return self.__cpf
 # setter = definidor
     # serve para alterar o valor do atributo; a grande vantagem dele é que você pode criar regras de validação antes de aceitar o novo valor
+
 # os métodos podem fazer já todo o trabalho de validar e afins, mas se você precisar apenas ler o saldo sem imprimir um textão de um método? nesse caso vc entra com o getter
