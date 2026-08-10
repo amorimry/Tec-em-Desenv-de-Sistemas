862e97be8e1e79dd326cf3e99cf11095dab885f5
amorimry <274507430+amorimry@users.noreply.github.com>
2026-08-10 20:00:52 -0300

    update

--- Módulo 01/UC-05/2026.08.10/tela_principal.py
+++ Módulo 01/UC-05/2026.08.10/tela_principal.py
@@ -8,12 +8,38 @@ app.title("Login")
 app.geometry("400x350") # largura e altura em pixel da tela que vai ser aberta
 
 # título
-titulo = ctk.CTkLabel(app, text="hello word!")
+titulo = ctk.CTkLabel(app, text="hello word!", font=("Arial", 24, "bold"))
 # label é um texto que aparece na janela
 # app é onde a label vai aparecer
 # text é o texto que vai aparecer na label
-titulo.pack()
+# na fonte foi três parâmetros
+titulo.pack(pady=(50,10))
 # variavel.pack() serve para o objeto aparecer na tela
+# no pack da tbm para passar parâmetros
+    # pady é o espaçamento, em pixel, o primeiro valor é o eixo x e o segundo é o eixo y
+    # mas oq foi anotado ai é um par de eixo x (eu acho)
 
+# subtítulo (uma label abaixo do título)
+subtitulo = ctk.CTkLabel(app, text="ciência da computação...")
+subtitulo.pack(pady=(10, 20))
+# esses valores é tipo, em cima vai espaçar 30 pixels e em baixo vai espaçar 50 pixels
+
+# campo do usuário
+entrada_user = ctk.CTkEntry(app, placeholder_text="semestre atual")
+# esse entry é o input
+# o app é onde vai abrir oq eu to fazendo
+# o placeholder_text é para colocar algo dentro do campo que o usuário está digitando
+entrada_user.pack(pady=(10, 10))
+
+# campo de senha
+entrada_senha = ctk.CTkEntry(app, placeholder_text="senha")
+# esse entry é o input
+# o app é onde vai abrir oq eu to fazendo
+# o placeholder_text é para colocar algo dentro do campo que o usuário está digitando
+entrada_senha.pack(pady=(10, 10))
+
+# botão do login
+botao_login = ctk.CTkButton(app, text="Entrar")
+botao_login.pack(pady=(10, 10))
 
 app.mainloop() # manter a janela aberta
