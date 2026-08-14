4bcf42b3bd929015eebdda785aa2b20357b13bc0
Ryan <274507430+amorimry@users.noreply.github.com>
2026-08-14 10:39:49 -0300

    aplicando a geometria nas coisas

---
+++ Módulo 01/UC05 - Aplicações Desktop/resumo geral/1. Janela principal/geometria.py
@@ -0,0 +1,29 @@
+import customtkinter as ctk
+
+app = ctk.CTk()
+app.title("Calculadora de IMC")
+app.geometry("450x300")
+app._set_appearance_mode('dark') # temas: light, dark, system
+
+titulo = ctk.CTkLabel(app, text='Calcule seu IMC', font=('arial', 20, 'bold'),text_color= '#ffdcff')
+titulo.grid(row=0, column=0, columnspan=2, pady=20)
+
+# text_color escolhe a cor do texto
+
+altura_titulo = ctk.CTkLabel(app, text='Altura', font=('comfortaa', 15, 'bold'), text_color='#ffb8ff')
+altura_titulo.grid(row=1, column=0, padx=20, sticky='w')
+
+altura_entry = ctk.CTkEntry(app, placeholder_text='Digite sua altura...', width=300, height=30, corner_radius=5,border_width=1, border_color='#ff63ff', text_color='#ff00ff', placeholder_text_color='#ffdcff')
+altura_entry.grid(row=1, column=1, pady=15)
+
+peso_titulo = ctk.CTkLabel(app, text='Peso', font=('comfortaa', 15, 'bold'), text_color='#ffb8ff')
+peso_titulo.grid(row=2, column=0, padx=20, sticky='w')
+
+peso_entry = ctk.CTkEntry(app, placeholder_text='Digite seu peso...', width=300, height=30, corner_radius=5,border_width=1, border_color='#ff63ff', text_color='#ff00ff', placeholder_text_color='#ffdcff')
+peso_entry.grid(row=2, column=1, pady=15)
+
+botao_calcular = ctk.CTkButton(app, text='Calcular', fg_color='#990099', hover_color='#FF99FF', border_color='#330033')
+botao_calcular.grid(row=4, column=1, pady=15)
+
+
+app.mainloop()
